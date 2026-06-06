50aaa187f2ed08955b6d2ff61680bf60aaeb14ec
Elziu <jan.jakub.mielziuk@gmail.com>
2026-06-06 10:03:19 +0200

    RS-4 Rejestracja modeli w Django Admin

--- src/booking/admin.py
+++ src/booking/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Room, Reservation
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "status")
+    list_filter = ("status",)
+    search_fields = ("name",)
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "room",
+        "date",
+        "start_time",
+        "end_time",
+        "status",
+        "created_by",
+    )
+    list_filter = ("status", "date", "room")
+    search_fields = ("room__name", "created_by__username")
