2abd7eba10f4e25c6fae8a50ace005ca73f4abaf
specsxc <piotr.dolgoszyja1@gmail.com>
2026-06-10 17:57:01 +0200

    RS-12 Dodano możliwość edycji oraz anulowania rezerwacji przez właściciela albo admina

--- src/booking/urls.py
+++ src/booking/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import create_reservation
+from .views import create_reservation, edit_reservation, cancel_reservation
 
 urlpatterns = [
     path("create/", create_reservation, name="create_reservation"),
+    path("<int:reservation_id>/edit/", edit_reservation, name="edit_reservation"),
+    path("<int:reservation_id>/cancel/", cancel_reservation, name="cancel_reservation"),
 ]
